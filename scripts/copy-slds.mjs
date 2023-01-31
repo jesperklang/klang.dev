c2626243805754bcd290108be60cbf03fa82db45
Jesper Klang <jesper.klang@sveasolar.com>
2023-01-31 22:05:21 +0100

    Fixing style copy

--- scripts/copy-slds.mjs
+++ scripts/copy-slds.mjs
@@ -1,4 +1,4 @@
 import cpx from 'cpx';
-cpx.copy('../../node_modules/@salesforce-ux/design-system/assets/**/*', 'src/assets', () => {
+cpx.copy('node_modules/@salesforce-ux/design-system/assets/**/*', 'src/assets', () => {
     console.log('Done copying SLDS resources');
 });
