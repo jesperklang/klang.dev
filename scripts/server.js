0fae748ed42dddb75c5d9e5c4672d779d97be4ae
Jesper Klang <jesper.klang@sveasolar.com>
2023-02-05 13:53:12 +0100

    Added helmet

--- scripts/server.js
+++ scripts/server.js
@@ -1,4 +1,5 @@
 import { createServer } from "lwr";
+import helmet from "helmet";
 
 const PORT = parseInt(process.env.PORT || "3001", 10);
 const SERVER_MODE = process.env.NODE_ENV || "prod";
@@ -11,6 +12,42 @@ const lwrServer = createServer({
 
 // Get the internal express app
 const expressApp = lwrServer.getInternalServer("express");
+
+expressApp.enable("trust proxy");
+expressApp.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                scriptSrcAttr: ["'unsafe-inline'"],
+                scriptSrc: [
+                    "'self'",
+                    "https://apis.google.com",
+                    "https://www.google.com",
+                    "https://www.gstatic.com",
+                    "'unsafe-inline'",
+                ],
+                childSrc: [
+                    "'self'",
+                    "https://www.google.com",
+                    "https://accounts.google.com/",
+                ],
+                frameAncestors: [
+                    "'self'",
+                    "https://jesperklang-dev-ed.my.salesforce.com",
+                    "https://klangdev-dev-ed.my.salesforce.com/",
+                ],
+                imgSrc: [
+                    "'self'",
+                    "https://ssl.gstatic.com",
+                    "https://csi.gstatic.com",
+                ],
+            },
+        },
+        noSniff: false,
+    })
+);
 
 expressApp.get("/api/hello", (req, res) => {
     res.json({ greeting: "Hello!" });
